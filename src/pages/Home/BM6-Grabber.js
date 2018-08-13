28e1a1f79bbb088ebe78abc5492c72907502270a
Cole Johnson <colejohnson@Coles-MacBook-Pro.local>
2018-08-12 18:50:45 -0700

    config

---
+++ src/pages/Home/BM6-Grabber.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import './_pillar.bm6_grabber.source.scss';
+
+class BM6Grabber extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="p-bm6_grabber">Poop</div>;
+  }
+}
