3095a8c81d90b6c4b5e1b4a73d1c1d66a38d0678
Cole Johnson <chj@purdue.edu>
2018-10-08 15:58:49 -0400

    Refactor et al

--- src/pages/Application/ApplicationRSVPToggle.js
+++ src/pages/Application/ApplicationRSVPToggle.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Button } from 'bm-kit';
+import Hr from '../../components/Hr';
 import {
   changeApplicationFieldValue,
   saveApplication
@@ -18,30 +19,32 @@ const ApplicationRSVPToggle = ({
   isLoading,
   styles
 }) => (
-  <div>
-    <Button
-      className="c_button p-application-toggle__button"
-      type="button"
-      disabled={applicationForm.rsvp === 1 || isLoading}
-      onClick={() => {
-        changeApplicationFieldValue('rsvp', 1);
-        saveApplication(true);
-      }}
-    >
-      I'll be there!
-    </Button>
-    <Button
-      className="c_button p-application-toggle__button"
-      type="button"
-      disabled={applicationForm.rsvp === 0 || isLoading}
-      onClick={() => {
-        changeApplicationFieldValue('rsvp', 0);
-        saveApplication(true);
-      }}
-    >
-      I can't make it
-    </Button>
-    <br />
+  <div className="p-rsvp_toggle">
+    <Hr>Can you make it BoilerMake?</Hr>
+    <div className="text-center">
+      <Button
+        className="c_button p-application-toggle__button"
+        type="button"
+        disabled={applicationForm.rsvp === 1 || isLoading}
+        onClick={() => {
+          changeApplicationFieldValue('rsvp', 1);
+          saveApplication(true);
+        }}
+      >
+        I'll be there!
+      </Button>
+      <Button
+        className="c_button p-application-toggle__button"
+        type="button"
+        disabled={applicationForm.rsvp === 0 || isLoading}
+        onClick={() => {
+          changeApplicationFieldValue('rsvp', 0);
+          saveApplication(true);
+        }}
+      >
+        I can't make it
+      </Button>
+    </div>
   </div>
 );
 
